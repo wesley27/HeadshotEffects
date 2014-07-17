914c77e821aa4af742f1e30dcc3ae592cf5c2663
wesley27 <www.wesley2@gmail.com>
2014-07-17 00:14:21 -0400

    recode/fixes/cleanup

--- src/com/wesley27/headshotsounds/headshotsounds.java
+++ src/com/wesley27/headshotsounds/headshotsounds.java
@@ -2,11 +2,12 @@ package com.wesley27.headshotsounds;
 
 import java.util.logging.Logger;
 
-import org.bukkit.Effect;
-import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -14,42 +15,56 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class headshotsounds extends JavaPlugin implements Listener {
-	
+
 	Logger logger = Logger.getLogger("Minecraft");
-	
+
 	public void onEnable() {
 		logger.info("HeadshotSounds Enabled");
 		getServer().getPluginManager().registerEvents(this, this);
 	}
-	
+
 	public void onDisable() {
 		logger.info("HeadshotSounds Disabled");
 	}
-	
-	@EventHandler
-	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
-		if(event.getCause() != DamageCause.ENTITY_ATTACK) {
+
+	@EventHandler(ignoreCancelled = true)
+	public void onHeadShot(EntityDamageByEntityEvent event) {
+		if(event.getCause() != DamageCause.PROJECTILE) {
 			return;
 		}
 		
-		Player shooter = (Player) event.getDamager();
+		Projectile proj = (Projectile)event.getDamager();
 		
-		if(!(shooter.getLastDamageCause() instanceof Player)) {
+		if(!(proj.getShooter() instanceof Player)) {
 			return;
 		}
 		
-		Entity arw = event.getEntity();
+		Entity victim = event.getEntity();
+		EntityType victimType = victim.getType();
 		
-		Location l = shooter.getLocation();
-		
-		double y = shooter.getLocation().getY();
-		double arwy = arw.getLocation().getY();
-		boolean headshot = y - arwy > 1.40D;
+		double projy = proj.getLocation().getY();
+		double victimy = victim.getLocation().getY();
+		boolean headshot = projy - victimy > getBodyheight(victimType);
 		
 		if(headshot) {
-			World world = shooter.getWorld();
-			world.playEffect(l,Effect.ZOMBIE_DESTROY_DOOR,0);
+			Entity p = victim;
+			World w = p.getWorld();
+			w.playSound(p.getLocation(), Sound.ZOMBIE_WOODBREAK, 2, 1);
+		}
+		
+	}
+	
+	private double getBodyheight(EntityType type) {
+		switch(type) {
+		case CREEPER:
+		case ZOMBIE:
+		case SKELETON:
+		case PLAYER:
+		case PIG_ZOMBIE:
+		case VILLAGER:
+			return 1.35d;
+		default:
+			return Float.POSITIVE_INFINITY;
 		}
 	}
-
 }
