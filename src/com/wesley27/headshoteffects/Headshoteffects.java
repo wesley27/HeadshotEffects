efd03ff21210398ef0c0993b3ef29cfe67ca2585
wesley27 <www.wesley2@gmail.com>
2014-12-11 23:22:38 -0500

    Uncommitted changes/updates

--- src/com/wesley27/headshoteffects/Headshoteffects.java
+++ src/com/wesley27/headshoteffects/Headshoteffects.java
@@ -8,43 +8,52 @@ import org.bukkit.World;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Headshoteffects extends JavaPlugin implements Listener {
+public class Headshoteffects extends JavaPlugin implements Listener 
+{
 
 	Logger logger = Logger.getLogger("Minecraft");
 
-	public void onEnable() {
+	public void onEnable() 
+	{
 		logger.info("HeadshotEffects Enabled");
 		getServer().getPluginManager().registerEvents(this, this);
 		
 		loadConfiguration();
 	}
 
-	public void onDisable() {
+	public void onDisable() 
+	{
 		logger.info("HeadshotEffects Disabled");
 	}
 	
-	public void loadConfiguration() {
+	public void loadConfiguration()
+	{
 		getConfig().options().copyDefaults(true);
 		saveDefaultConfig();
 	}
 
 	@EventHandler(ignoreCancelled = true)
-	public void onHeadShot(EntityDamageByEntityEvent event) {
-		if(event.getCause() != DamageCause.PROJECTILE) {
+	public void onHeadShot(EntityDamageByEntityEvent event) 
+	{
+		if(event.getCause() != DamageCause.PROJECTILE) 
+		{
 			return;
 		}
 		
 		Projectile proj = (Projectile)event.getDamager();
 		
-		if(!(proj.getShooter() instanceof Player)) {
+		if(!(proj.getShooter() instanceof Player)) 
+		{
 			return;
 		}
 		
@@ -61,50 +70,109 @@ public class Headshoteffects extends JavaPlugin implements Listener {
 				return;
 			}
 			Player shooter = (Player)proj.getShooter();
-			Player victimn = (Player)victim;
+			LivingEntity victimn = (LivingEntity)victim;
+			Player victima = (Player)victimn;
 			World w = shooter.getWorld();
 			
-			String vnamevar = victimn.getName();
+			String vnamevar = victima.getName();
 			String snamevar = shooter.getName();
 			
-			if(!(getConfig().getString("ShooterMessage")).equalsIgnoreCase("none")) {
-				String shootermsg = ChatColor.translateAlternateColorCodes('&', getConfig().getString("ShooterMessage"));
+			if(getConfig().getBoolean("UseSpecificWorlds"))
+			{
+				if(getConfig().getStringList("Worlds").contains(w.getName()))
+				{
+					if(!(getConfig().getString("ShooterMessage")).equalsIgnoreCase("none")) 
+					{
+						String shootermsg = ChatColor.translateAlternateColorCodes('&', getConfig().getString("ShooterMessage"));
+						
+						shooter.sendMessage(shootermsg.replace("%victim", vnamevar));
+					}
+					
+					if(getConfig().getBoolean("InstaKill")) 
+					{
+						if(event.getDamager() instanceof Snowball)
+						{
+							event.setDamage(20);
+						}
+					}
+					
+					if(!(getConfig().getString("HeadshotSound")).equalsIgnoreCase("none")) 
+					{
+						try {
+							Sound hssound = Sound.valueOf(getConfig().getString("HeadshotSound"));
+							w.playSound(shooter.getLocation(), hssound, 2, 1);
+						}
+						catch(IllegalArgumentException invalidsound) 
+						{
+							shooter.sendMessage(ChatColor.RED + "[HeadshotSounds] An invalid sound name was entered in the config, notify an admin!");
+						}
+					}
+					
+					if(!(getConfig().getString("RunCommand")).equalsIgnoreCase("none"))
+					{
+						String runcmd = ChatColor.translateAlternateColorCodes('&', getConfig().getString("RunCommand"));
+						
+						getServer().dispatchCommand(getServer().getConsoleSender(), runcmd
+								.replace("%shooter", snamevar)
+								.replace("%victim", vnamevar)
+								);
+					}
+					
+					else 
+					{
+						return;
+					}
+				}
+			}
+			else
+			{
+				if(!(getConfig().getString("ShooterMessage")).equalsIgnoreCase("none")) 
+				{
+					String shootermsg = ChatColor.translateAlternateColorCodes('&', getConfig().getString("ShooterMessage"));
+					
+					shooter.sendMessage(shootermsg.replace("%victim", vnamevar));
+				}
 				
-				shooter.sendMessage(shootermsg.replace("%victim", vnamevar));
-			}
-			
-			if(!(getConfig().getString("InstaKill")).equalsIgnoreCase("false")) {
-				if(event.getDamager() instanceof Arrow) {
-					event.setDamage(20);
+				if(getConfig().getBoolean("InstaKill")) 
+				{
+					if(event.getDamager() instanceof Arrow)
+					{
+						event.setDamage(20);
+					}
 				}
-			}
-			
-			if(!(getConfig().getString("HeadshotSound")).equalsIgnoreCase("none")) {
-				try {
-					Sound hssound = Sound.valueOf(getConfig().getString("HeadshotSound"));
-					w.playSound(shooter.getLocation(), hssound, 2, 1);
-				}
-				catch(IllegalArgumentException invalidsound) {
-					shooter.sendMessage(ChatColor.RED + "[HeadshotSounds] An invalid sound name was entered in the config, notify an admin!");
-				}
-			}
-			
-			if(!(getConfig().getString("RunCommand")).equalsIgnoreCase("none")) {
-				String runcmd = ChatColor.translateAlternateColorCodes('&', getConfig().getString("RunCommand"));
 				
-				getServer().dispatchCommand(getServer().getConsoleSender(), runcmd
-						.replace("%shooter", snamevar)
-						.replace("%victim", vnamevar)
-						);
-			}
-			
-			else {
-				return;
+				if(!(getConfig().getString("HeadshotSound")).equalsIgnoreCase("none")) 
+				{
+					try {
+						Sound hssound = Sound.valueOf(getConfig().getString("HeadshotSound"));
+						w.playSound(shooter.getLocation(), hssound, 2, 1);
+					}
+					catch(IllegalArgumentException invalidsound) 
+					{
+						shooter.sendMessage(ChatColor.RED + "[HeadshotSounds] An invalid sound name was entered in the config, notify an admin!");
+					}
+				}
+				
+				if(!(getConfig().getString("RunCommand")).equalsIgnoreCase("none"))
+				{
+					String runcmd = ChatColor.translateAlternateColorCodes('&', getConfig().getString("RunCommand"));
+					
+					getServer().dispatchCommand(getServer().getConsoleSender(), runcmd
+							.replace("%shooter", snamevar)
+							.replace("%victim", vnamevar)
+							);
+				}
+				
+				else 
+				{
+					return;
+				}
 			}
 		}		
 	}
 	
-	private double getBodyheight(EntityType type) {
+	private double getBodyheight(EntityType type) 
+	{
 		switch(type) {
 		case CREEPER:
 		case ZOMBIE:
